0d880fe3d4afed685674641e4fbd7117b22d8838
Marcelo <nossamarcelo@gmail.com>
2016-09-23 09:51:15 -0300

    #ChangeLog Candidato Controller

---
+++ PesquisaEleitoral/Controllers/CandidatosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PesquisaEleitoral.Models;
+
+namespace PesquisaEleitoral.Controllers
+{
+    public class CandidatosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Candidatos
+        public ActionResult Index()
+        {
+            return View(db.CandidatoContext.ToList());
+        }
+
+        // GET: Candidatos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidato candidato = db.CandidatoContext.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        // GET: Candidatos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidatos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CandidatoId,CandidatoNome,CandidatoNumero")] Candidato candidato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CandidatoContext.Add(candidato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(candidato);
+        }
+
+        // GET: Candidatos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidato candidato = db.CandidatoContext.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        // POST: Candidatos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CandidatoId,CandidatoNome,CandidatoNumero")] Candidato candidato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(candidato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(candidato);
+        }
+
+        // GET: Candidatos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidato candidato = db.CandidatoContext.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        // POST: Candidatos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Candidato candidato = db.CandidatoContext.Find(id);
+            db.CandidatoContext.Remove(candidato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
