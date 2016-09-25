ef166bdc071f04ee17e3bb37268b2b6a7849a291
Marcelo <nossamarcelo@gmail.com>
2016-09-25 18:39:14 -0300

    #ChangeLog Voto

---
+++ PesquisaEleitoral/Controllers/VotoController.cs
@@ -0,0 +1,38 @@
+﻿using PesquisaEleitoral.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PesquisaEleitoral.Controllers
+{
+    public class VotoController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Voto()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Voto(VotoViewModels model)
+        {
+            ApplicationUser user = new ApplicationUser();
+            user.Email = System.Web.HttpContext.Current.User.Identity.Name;
+            user = VerificarUsuarioPorEmail(user);
+            user.CandidatoID = model.CandidatoNumero;
+            if(user.CandidatoID == 0)
+            {
+                HttpNotFound();
+            }else
+            {
+                db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            
+            return RedirectToAction("Voto");
+        }
+        public ApplicationUser VerificarUsuarioPorEmail(ApplicationUser u)
+        {
+            return db.Users.FirstOrDefault(x => x.Email.Equals(u.Email));
+        }
+    }
+}
