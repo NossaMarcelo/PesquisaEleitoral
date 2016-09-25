9ec119c81c4aa5e6954caff6a2fd605f0e163659
Marcelo <nossamarcelo@gmail.com>
2016-09-25 19:04:07 -0300

    #ChangeLog CandidatoValidações

--- PesquisaEleitoral/Models/Candidato.cs
+++ PesquisaEleitoral/Models/Candidato.cs
@@ -14,7 +14,19 @@ namespace PesquisaEleitoral.Models
         public int CandidatoId { get; set; }
         [Required]
         [RegularExpression(@"^[aA-zZ]+((\s[aA-zZ]+)+)?$", ErrorMessage = "Não é permitido o uso de acentos, caracteres especiais e espaços extras.")]
-        public string CandidatoNome { get; set; }
+        public string CandidatoNome
+        {
+            get
+            {
+
+                return this._nome;
+            }
+            set
+            {
+                this._nome = value.Trim().ToUpper();
+            }
+        }
+        private string _nome;
         [Required]
         [Range(10,99, ErrorMessage = "Número do candidato inválido.")]
         public int CandidatoNumero { get; set; }
